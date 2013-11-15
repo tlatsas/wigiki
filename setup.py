d1c83ea9dabf4f1eff28a8f410708142d7468ab7
Tasos Latsas <tlatsas@gmx.com>
2013-11-15 23:17:17 +0200

    add static files

--- setup.py
+++ setup.py
@@ -29,11 +29,30 @@ Example:
 
 """
 
+import os
+import sys
 from setuptools import setup
 import wigiki
 
 with open("requirements.txt") as fp:
     requirements = fp.read().splitlines()
+
+# default template files
+tpl_path = "wigiki"
+if os.name is 'posix':
+    if sys.prefix.split(os.sep)[1] == 'usr':
+        tpl_path = "/usr/share/wigiki"
+
+tpl_files = [
+    (os.path.join(tpl_path, "templates", "default"),
+        ["templates/default/index.html",
+        "templates/default/base.html",
+        "templates/default/page.html"]
+    ),
+    (os.path.join(tpl_path, "templates", "default", "assets"),
+        ["templates/default/assets/style.css"]
+    )
+]
 
 setup(
     name="wigiki",
@@ -52,7 +71,7 @@ setup(
         ],
     },
     classifiers = [
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Intended Audience :: End Users/Desktop",
@@ -62,4 +81,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Topic :: Internet",
     ],
+    data_files=tpl_files
 )

84f5e3af9c08ed1fd539ac78c2231c86df6fe92f
Tasos Latsas <tlatsas@gmx.com>
2013-11-25 20:34:49 +0200

    fix small typo

--- wigiki/__init__.py
+++ wigiki/__init__.py
@@ -1,3 +1,3 @@
-"""A static site gemerator which uses Github's gists as pages"""
+"""A static site generator which uses Github's gists as pages"""
 __author__ = "Tasos Latsas"
 __version__ = "0.6.2"
